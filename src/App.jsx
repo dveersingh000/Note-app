cd13029b55866b639e34f90ceb57164680d06cc9
Dharamveer <dveersingh000@gmail.com>
2024-08-28 16:08:36 +0530

    implemented first page basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
 import './App.css'
+import img1 from './assets/img1.png';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      
-    </>
+    <div className='app'>
+      <div className="sidebar">
+        <h3>Pocket Notes</h3>
+        <button className='btn'>+</button>
+      </div>
+      <div className="container">
+        <img src={img1} alt="logo" />
+        <h2>Pocket Notes</h2>
+        <p>Send and receive messages without keeping your phone online.
+          <br />
+        Use Pocket Notes on up to 4 linked devices and 1 mobile phone</p>
+        <div className="footer">
+          <p>🔒end-to-end encrypted</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
